da2ea205e077789b6ad8d55b19c47364e2b2d391
codeaadi <adityadubey362@gmail.com>
2023-02-16 08:28:03 +0530

    added: Addressbook and contact method

---
+++ com/bridgelabz/AddressBook/AddressBook.java
@@ -0,0 +1,37 @@
+package com.bridgelabz.AddressBook;
+
+import java.util.ArrayList;
+
+public class AddressBook {
+    private ArrayList<ContactPerson> contactBook ;
+
+    public AddressBook() {
+        this.contactBook = contactBook;
+    }
+
+    public ArrayList<ContactPerson> getContactBook() {
+        return contactBook;
+    }
+
+    public void setContactBook(ArrayList<ContactPerson> contactBook) {
+        this.contactBook = contactBook;
+    }
+
+    public void addContacts() {
+    }
+
+    public void edit() {
+    }
+
+    public void display() {
+    }
+
+    public void delete() {
+    }
+
+    public void searchByCity(String cityName) {
+    }
+
+    public void searchByState(String stateName) {
+    }
+}
